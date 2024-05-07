c103dbc23129fe863450de54d959ae13266101e7
Abhishek Kumar <abprajapati3333@gmail.com>
2024-05-07 07:55:44 +0530

    firebase integration completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:chitra_app/views/screens/category.dart';
 import 'package:chitra_app/views/screens/homeScreen.dart';
-import 'package:chitra_app/views/screens/search_screen.dart';
 import 'package:chitra_app/views/screens/splashScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 void main()
-{
+async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
